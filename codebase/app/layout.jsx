2963c71ee88e8890aa2d566faa450478f190d401
Parisha <parisha.workshop@gmail.com>
2023-05-15 17:16:19 +0530

    Fixed errors and added index.js integrated with _app.js

--- codebase/app/layout.jsx
+++ codebase/app/layout.jsx
@@ -1,16 +1,11 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
-import { metadata } from './metadata';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
