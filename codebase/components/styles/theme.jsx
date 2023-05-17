10e17a8d40bca0fc1562caf9bf646842c5feeed7
Parisha <parisha.workshop@gmail.com>
2023-05-17 20:08:28 +0530

    theme updated

--- codebase/components/styles/theme.jsx
+++ codebase/components/styles/theme.jsx
@@ -1,25 +1,28 @@
-// Defining theme
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
-  primary: {
-  main: '#586BA4', // Indigo
-  },
-  secondary: {
-  main: '#324376', // Dark Blue
-  },
-  tertiary: {
-  main: '#F5DD90', // Pale Gold
-  },
-  quaternary: {
-  main: '#F68E5F', // Salmon
-  },
-  quinary: {
-  main: '#86608E', // Royal Purple
-  },
+    primary: {
+      main: '#ececec', // Platinum
+    },
+    secondary: {
+      main: '#f3d250', // Gold Crayola
+    },
+    tertiary: {
+      main: '#f78888', // Light Coral 
+    },
+    quaternary: {
+      main: '#90ccf4', // Maya Blue
+    },
+    quinary: {
+      main: '#5da2d5', // Air Force Blue (RAF)
+    },
+    text: {
+      primary: '#000000', // Black font color
+    },
+    mode: 'light', // Initial mode set to light
   },
   // Add more options to your theme if needed
-  });
+});
 
 export default theme;
