6d12a879b1ed9e0e76b9100741f3b4ea47aab61a
Parisha <parisha.workshop@gmail.com>
2023-05-17 16:26:40 +0530

    Updated with colors

---
+++ codebase/components/styles/theme.jsx
@@ -0,0 +1,25 @@
+// Defining theme
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+  primary: {
+  main: '#586BA4', // Indigo
+  },
+  secondary: {
+  main: '#324376', // Dark Blue
+  },
+  tertiary: {
+  main: '#F5DD90', // Pale Gold
+  },
+  quaternary: {
+  main: '#F68E5F', // Salmon
+  },
+  quinary: {
+  main: '#86608E', // Royal Purple
+  },
+  },
+  // Add more options to your theme if needed
+  });
+
+export default theme;
