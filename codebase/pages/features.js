bc5bc83275224dbd231cede428e27ba1002e2486
Parisha <parisha.workshop@gmail.com>
2023-05-18 13:10:49 +0530

    minor edits to page templates

--- codebase/pages/features.js
+++ codebase/pages/features.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image'
-import { metadata } from '../components/metadata';
 import Link from 'next/link'
 
 export default function Features() {
