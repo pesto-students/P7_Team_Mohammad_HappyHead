a16607e57609c24406a52ee9f70e15073cc315be
Parisha <parisha.workshop@gmail.com>
2023-05-19 17:09:40 +0530

    Formatting

--- codebase/components/Home/HeroComponent.jsx
+++ codebase/components/Home/HeroComponent.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
-import { Button, Typography } from '@mui/material';
-import { styled, ThemeProvider } from '@mui/system';
-import Face2OutlinedIcon from '@mui/icons-material/Face2Outlined';
-import FaceOutlinedIcon from '@mui/icons-material/FaceOutlined';
-import Face4OutlinedIcon from '@mui/icons-material/Face4Outlined';
-import Face5OutlinedIcon from '@mui/icons-material/Face5Outlined';
+import React from 'react'
+import { Button, Typography } from '@mui/material'
+import { ThemeProvider } from '@mui/system'
+import Face2OutlinedIcon from '@mui/icons-material/Face2Outlined'
+import FaceOutlinedIcon from '@mui/icons-material/FaceOutlined'
+import Face4OutlinedIcon from '@mui/icons-material/Face4Outlined'
+import Face5OutlinedIcon from '@mui/icons-material/Face5Outlined'
 
-import theme from '../styles/theme';
-import RootContainer from './RootContainer';
-import ContentContainer from './ContentContainer';
-import IconContainer from './IconContainer';
-import Title from './Title';
-import Subtitle from './Subtitle';
-import ButtonWrapper from './ButtonWrapper';
+import theme from '../styles/Theme'
+import RootContainer from '../styles/RootContainerStyles'
+import ContentContainer from '../styles/ContentContainerStyles'
+import IconContainer from '../styles/IconContainerStyles'
+import Title from '../styles/TitleStyles'
+import Subtitle from '../styles/SubtitleStyles'
+import ButtonWrapper from '../styles/ButtonWrapperStyles'
 
 const HeroComponent = () => {
   return (
     <ThemeProvider theme={theme}>
+      {/* Custom root container */}
       <RootContainer>
+        {/* Custom content container */}
         <ContentContainer>
+          {/* Title */}
           <Title variant="h2">Your Chief Happiness Officer</Title>
+
+          {/* Subtitle */}
           <Subtitle variant="h3">HappyHead</Subtitle>
+
+          {/* Icon container */}
           <IconContainer>
+            {/* Icons */}
             <Face2OutlinedIcon fontSize="large" />
             <FaceOutlinedIcon fontSize="large" />
             <Face4OutlinedIcon fontSize="large" />
             <Face5OutlinedIcon fontSize="large" />
           </IconContainer>
-          <ButtonWrapper>
+
+          {/* Button to explore tools */}
+          <ButtonWrapper color="primary">
             <Button variant="contained" color="quaternary">
               Explore Tools
             </Button>
           </ButtonWrapper>
-          <ButtonWrapper>
+
+          {/* Button to register as an expert */}
+          <ButtonWrapper color="tertiary">
             <Button variant="contained" color="tertiary">
               Register as an Expert
             </Button>
@@ -43,4 +55,4 @@ const HeroComponent = () => {
   );
 };
 
-export default HeroComponent;
+export default HeroComponent
