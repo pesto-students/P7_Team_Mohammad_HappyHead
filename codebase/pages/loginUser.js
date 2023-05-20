48a7922f5c8612e0e3fed8c41730a26c37e32a3e
Parisha <parisha.workshop@gmail.com>
2023-05-20 17:24:30 +0530

    template added for page

---
+++ codebase/pages/loginUser.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = () => {
+  // Your login page content and logic here
+
+  return (
+    <div>
+      Your Login page here
+    </div>
+  );
+};
+
+export default LoginPage;
