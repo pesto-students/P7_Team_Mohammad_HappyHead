5d89a5be061c5310fba3eba41e64c9a960d9f18e
Parisha <parisha.workshop@gmail.com>
2023-05-20 16:38:44 +0530

    template to check if the current user is logged in added

---
+++ codebase/pages/check-login.js
@@ -0,0 +1,11 @@
+const checkLoginHandler = (req, res) => {
+    // Implement your logic to check the login status of the user here
+    // You can access the request object (req) to get any necessary information
+  
+    // Example logic:
+    const isLoggedIn = true; // Replace with your actual login status checking logic
+  
+    res.status(200).json({ isLoggedIn });
+  };
+  
+  export default checkLoginHandler;
