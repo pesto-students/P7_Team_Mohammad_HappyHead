e6d3cf3cac9b6450884381c8ed216eb4eefab1f8
Parisha <parisha.workshop@gmail.com>
2023-05-21 00:37:09 +0530

    moved tools path and description to a file

---
+++ codebase/components/Features/toolsData.js
@@ -0,0 +1,20 @@
+export const toolsData = [
+    {
+      name: 'You Mental Health Report',
+      subtext:
+        'Answer a few questions related to your stressors & lifestyle and get personalized insights and recommendations',
+      path: '/questionnaire',
+    },
+    {
+      name: 'Connect with Experts',
+      subtext:
+        'Get personalized guidance and support from certified professionals to address your mental health concerns effectively.',
+      path: '/connect-with-experts',
+    },
+    {
+      name: 'Guided Practice Tools',
+      subtext: `Discover effective relaxation techniques and practices to enhance your well-being and find inner calm amidst life's challenges.`,
+      path: '/practice-tools',
+    },
+  ];
+  
