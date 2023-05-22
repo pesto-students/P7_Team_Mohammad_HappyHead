f557d4c42bf3c1856965adb787d0004bc4e9e431
Parisha <parisha.workshop@gmail.com>
2023-05-22 15:26:10 +0530

    moved to pages/users route

---
+++ codebase/pages/users/connect-with-experts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ExpertsPage from '../../components/ConnectWithExperts/Index'
+
+function ConnectWithExpertsPage() {
+  return (
+    <div>
+     <ExpertsPage/>
+    </div>
+  );
+}
+export default ConnectWithExpertsPage;
