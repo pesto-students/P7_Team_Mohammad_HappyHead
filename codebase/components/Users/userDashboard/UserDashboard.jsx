72aa2ee37b3235703fa437c6ce463b2491f3fbca
Parisha <parisha.workshop@gmail.com>
2023-05-23 15:29:33 +0530

    resolving warnings from build

--- codebase/components/Users/userDashboard/UserDashboard.jsx
+++ codebase/components/Users/userDashboard/UserDashboard.jsx
@@ -35,9 +35,9 @@ const CustomDialog = styled(Dialog)(({ theme }) => ({
         '& fieldset': {
             borderColor: theme.palette.text.primary,
         },
-    '& .MuiDialogTitle-root': {
-        color: theme.palette.text.primary,
-    },
+        '& .MuiDialogTitle-root': {
+            color: theme.palette.text.primary,
+        },
     },
 }));
 
@@ -80,24 +80,23 @@ const UserDashboard = ({ username, isLoggedIn }) => {
         setOpenDialog(false);
     };
 
-    // Fetch user profile data
-    const getUserProfile = async () => {
-      try {
-        const response = await fetch(`/api/users/${username}`);
-        const data = await response.json();
-        setUserProfile(data);
-        setEditedProfile(data);
-      } catch (error) {
-        console.error('Failed to fetch user profile', error);
-      }
-    };
-
     // Fetch user profile data when the component mounts
     useEffect(() => {
-      if (username) {
-        getUserProfile();
-      }
-    }, [username]);
+        // Fetch user profile data
+        const getUserProfile = async () => {
+            try {
+                const response = await fetch(`/api/users/${username}`);
+                const data = await response.json();
+                setUserProfile(data);
+                setEditedProfile(data);
+            } catch (error) {
+                console.error('Failed to fetch user profile', error);
+            }
+        };
+        if (username) {
+            getUserProfile();
+        }
+    }, [username, getUserProfile]);
 
     // Save the edited profile
     const handleSaveProfile = async () => {
