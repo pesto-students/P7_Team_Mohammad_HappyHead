11716a17469ae215c7659eae8ac940f39e22afc7
Parisha <parisha.workshop@gmail.com>
2023-05-23 13:39:00 +0530

    path errors resolved

--- codebase/pages/booking-history.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-import Image from 'next/image'
-import { metadata } from '../components/metadata';
-import Link from 'next/link'
-
-export default function BookingHistory() {
-    return (
-        <h1>BookingHistory Page</h1>
-    )}
