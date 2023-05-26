742dc9eb85b7d4a4401ebecb3632a746e77a37e0
Parisha <PARISHA.WORKSHOP@GMAIL.COM>
2023-05-26 16:05:07 +0530

    removed commented sections

--- codebase/pages/users/practicetools/[username].js
+++ codebase/pages/users/practicetools/[username].js
@@ -1,11 +1,7 @@
 import React from 'react';
-// import { useRouter } from 'next/router';
 import UserToolsPage from '../../../components/Users/practiceTools/Index';
 
 const ToolPageUser = () => {
-  // const router = useRouter();
-  // const { username } = router.query;
-
   return <UserToolsPage/>;
 };
 
