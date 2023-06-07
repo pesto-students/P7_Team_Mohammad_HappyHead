904662d4313abe61f4d7690f01ce519e443843ba
Parisha <PARISHA.WORKSHOP@GMAIL.COM>
2023-06-07 21:43:20 +0530

    dashboard styling updated

--- codebase/components/Users/userDashboard/Upcoming Appointments.jsx
+++ codebase/components/Users/userDashboard/Upcoming Appointments.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { styled, ThemeProvider } from '@mui/system';
-import { Typography, Card, CardContent } from '@mui/material';
+import { Typography, Card, CardContent, Grid } from '@mui/material';
 import RootContainer from '../../styles/RootContainerStyles';
 import SectionContainer from '../../styles/SectionsContainer';
 import IconContainer from '../../styles/IconContainerStyles';
@@ -10,36 +10,29 @@ import theme from '../../styles/theme';
 
 // Custom styled components for the root container, content container, and dialog
 const CustomRootContainer = styled(RootContainer)(({ theme }) => ({
-  padding: '0 2rem 0',
-}));
-
-// Styled component for the main content container
-const CustomSectionContainer = styled(SectionContainer)(({ theme }) => ({
-  margin: '0 2rem',
-  backgroundColor: theme.palette.primary.main,
-  width: '100%',
-}));
-
-const CustomTitle = styled(Typography)(({ theme }) => ({
-  ...theme.typography.h2,
-  [theme.breakpoints.down('sm')]: {
-    fontSize: theme.typography.h4.fontSize,
-  },
+  padding: '1rem 2rem 2rem 2rem',
 }));
 
 const CustomCard = styled(Card)(({ theme, cardColor }) => ({
   backgroundColor: cardColor,
   width: '100%',
   [theme.breakpoints.up('md')]: {
-    width: '80vw',
+    width: '60vw',
+    padding: '0 2rem',
   },
   margin: '0.5rem',
-  padding: '0 2rem',
   borderRadius: '8px',
   '&:hover': {
     cursor: 'pointer',
     boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)', // Update with desired box shadow style
     transform: 'scale(1.02)', // Update with desired transformation
+  },
+}));
+
+const CustomCardContent = styled(CardContent)(({ theme, cardColor }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 2fr',
   },
 }));
 
@@ -51,39 +44,20 @@ const StyledIconContainer = styled(IconContainer)(() => ({
   },
 }));
 
-// Custom styled component for the container
-const CustomContainer = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.tertiary.main,
-  padding: '2rem',
-  [theme.breakpoints.up('lg')]: {
-    paddingLeft: '7rem',
+const CustomTitle = styled(Typography)(({ theme }) => ({
+  ...theme.typography.h2,
+  padding: '1rem 0',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: theme.typography.h4.fontSize,
   },
 }));
 
-const CustomList = styled('ul')({
-  listStyleType: 'none',
-  padding: 0,
-});
-
-const CustomListItem = styled('li')({
-  display: 'inline-block',
-  border: '0.2rem solid white',
-  borderRadius: '8px',
-  background: 'transparent',
-  padding: '1rem',
-  marginBottom: '1rem',
-  [theme.breakpoints.up('lg')]: {
-    marginRight: '2rem',
-  },
-});
-
-const Heading = styled('h2')(({ theme }) => ({
-  textAlign: 'center',
-  paddingBottom: '1rem',
-  [theme.breakpoints.up('lg')]: {
-    textAlign: 'left',
-  },
-}));
+const SubText = styled(Typography)(({ theme }) => ({
+  fontFamily: theme.typography.h2.fontFamily,
+  marginBottom: theme.spacing(2),
+  marginTop: theme.spacing(2),
+  fontSize: '1.2rem',
+}))
 
 const UpcomingAppointments = () => {
   const router = useRouter();
@@ -130,35 +104,40 @@ const UpcomingAppointments = () => {
         {upcomingAppointments.length === 0 ? (
           noAppointmentsMessage
         ) : (
-          <CustomSectionContainer>
+          <>
             {upcomingAppointments.map((appointment, index) => (
               <CustomCard
-                key={`${appointment.date}-${index}`}
                 cardColor={cardColors[index % cardColors.length]}
-              >
-                <StyledIconContainer>
-                  <img src="/images/dashboard/appointment.png" alt="appointment" />
-                </StyledIconContainer>
-                <TextStyle>
-                  <strong>Date:</strong>{' '}
-                  {new Date(appointment.date).toLocaleDateString('en-US', {
-                    day: 'numeric',
-                    month: 'long',
-                    year: 'numeric',
-                  })}
-                </TextStyle>
-                <TextStyle>
-                  <strong>Time:</strong> {appointment.startTime} - {appointment.endTime}
-                </TextStyle>
-                <TextStyle>
-                  <strong>Expert Name:</strong> {appointment.user.name}
-                </TextStyle>
-                <TextStyle>
-                  <strong>Expert Phone Number:</strong> {appointment.user.phoneNumber}
-                </TextStyle>
+                key={`${appointment.date}-${index}`}>
+                <CustomCardContent>
+                  <StyledIconContainer>
+                    <img src="/images/dashboard/appointment.png" alt="appointment" />
+                  </StyledIconContainer>
+
+                  <div>
+                    <SubText>
+                      <strong>Date:</strong>{' '}
+                      {new Date(appointment.date).toLocaleDateString('en-US', {
+                        day: 'numeric',
+                        month: 'long',
+                        year: 'numeric',
+                      })}
+                    </SubText>
+                    <SubText>
+                      <strong>Time:</strong> {appointment.startTime} - {appointment.endTime}
+                    </SubText>
+                    <SubText>
+                      <strong>Expert Name:</strong> {appointment.user.name}
+                    </SubText>
+                    <SubText>
+                      <strong>Expert Phone Number:</strong> {appointment.user.phoneNumber}
+                    </SubText>
+                  </div>
+                </CustomCardContent>
               </CustomCard>
+
             ))}
-          </CustomSectionContainer>
+          </>
         )}
       </CustomRootContainer>
     </ThemeProvider>
