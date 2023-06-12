085e595722a8e231bf44555fa8ee542b1005a5ad
Parisha <PARISHA.WORKSHOP@GMAIL.COM>
2023-06-12 12:45:18 +0530

    minor edit

--- codebase/pages/api/signup.jsx
+++ codebase/pages/api/signup.jsx
@@ -1,21 +1,19 @@
 const { connectToDatabase } = require('../../utils/mongodb');
 const ObjectId = require('mongodb').ObjectId;
-import {hashPassword} from './authUtil';
-
+import { hashPassword } from './authUtil';
 
 export default async function SignUphandler(req, res) {
   if (req.method === 'POST') {
     try {
-      
       // Update the Contact us form inputs in the database
-      const { name, email, password } = req.body
-      
-      updateDBSignUp(name, email, password)
+      const { name, email, password } = req.body;
 
-      res.status(200).json({ message: 'Form submission successful!' })
+      await updateDBSignUp(name, email, password);
+
+      res.status(200).json({ message: 'Form submission successful!' });
     } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: 'Internal server error' })
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
     }
   } else {
     // Return a response with method not allowed status code for non-POST requests
@@ -23,16 +21,15 @@ export default async function SignUphandler(req, res) {
   }
 }
 
-export const updateDBSignup = async (name, email, password) => {
+const updateDBSignUp = async (name, email, password) => {
   // Connect to the MongoDB Atlas cluster
   let { db } = await connectToDatabase();
-  
-  const {hashedPassword, salt} = await hashPassword(password);
+
+  const { hashedPassword, salt } = await hashPassword(password);
 
   let user = await db.collection('Users').findOne({ email: email });
 
   if (!user) {
-    await db.collection('Users').insertOne({ name, email, hashedPassword, salt })
-
+    user = await db.collection('Users').insertOne({ name, email, hashedPassword, salt });
   }
-}
+};
