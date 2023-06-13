ed77485ce034cd7003c19f6661ad7ccb7c2df175
Parisha <PARISHA.WORKSHOP@GMAIL.COM>
2023-06-13 16:57:06 +0530

    logout page added

---
+++ codebase/pages/logout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoggedOut from '../components/Logout/Index'
+
+function SignIn() {
+  return (
+    <div>
+     <LoggedOut/>
+    </div>
+  );
+}
+export default SignIn
